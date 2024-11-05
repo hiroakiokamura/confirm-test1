122e3426a1ba0c0e27776b52617f44ce0bb4c612
hiroakiokamura <howillow12021985@gmail.com>
2024-11-05 17:50:41 +0900

    基本的な環境構築が完了したファイル

---
+++ src/resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TailwindCSS Test</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-gray-100 text-center p-5">
+    <h1 class="text-4xl font-bold text-blue-500">TailwindCSSが正しく動作しています！</h1>
+</body>
+
+</html>
